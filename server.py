ce3d5df4505bd765ab9d258b9ccffccf8d495920
Yasser Naji <yfnaji@gmail.com>
2023-06-16 01:48:49 +0100

    Add and use debug variable in server.py

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from index import layout
 from app import app
+import sys
 
+if sys.argv[1] == "debug":
+    print(f"arg {sys.argv[1]}")
+    debug = True
+else:
+    debug = False
 
 app.layout = layout
 srv = app.server
 app.title = "BlackDash"
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0')
+    app.run_server(host='0.0.0.0', debug=debug)
